c7e2512738c481b01d9bf1b861ba1e65cb835e1a
matsmy <101180563+matsmy@users.noreply.github.com>
2022-05-17 09:35:05 +0000

    3_1

--- Main.java
+++ Main.java
@@ -1,18 +1,35 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
     try {
-      Service1 s = new Service1();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
-
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
-      }
-    } catch (IOException e) {
-
+    Scanner scan = new Scanner(System.in);
+    Service1 s = new Service1();
+    int a = 0;
+    System.out.println("Wybierz działanie:\n1. Dodawanie nowego studenta\n2. Wyswietlenie listy studentow");
+    a=scan.nextInt();
+    scan.nextLine();
+    switch (a) {
+      case 1: {
+        System.out.println("Podaj imie");
+        String name = scan.nextLine();
+        System.out.println("Podaj wiek");
+        int age = scan.nextInt();
+        s.addStudent(new Student(name, age));
+        break;
     }
-  }
-}
+    case 2: {
+        System.out.println("Lista studentow\n");
+        var students = s.getStudents();
+        for(Student current : students)
+        {
+        System.out.println(current.ToString());
+        }
+        break;
+      }}}
+  
+      catch (IOException e) {
+      System.out.print("ERROR!!");
+        }
+    }}
